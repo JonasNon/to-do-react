d88a16d815a1d1f8b42c69fbd195b8ad757ce748
JonasNon <officialjonasn@gmail.com>
2024-08-06 20:49:24 -0500

    working with some extra non-working bits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import props from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isClicked: false,
+      inputValue: "",
+      stringList: [],
+      editMode: false
+    }
+  }
+  
+  handleClick = (index) => {
+    this.state.isClicked ? this.setState({isClicked : false}) : this.setState({isClicked : true})
+  }
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({stringList: [...this.state.stringList, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+  
+  deleteItem = (index) => {
+    let objectCopy = [...this.state.stringList]
+    objectCopy.splice(index, 1)
+    console.log(objectCopy)
+    this.setState({stringList: [...objectCopy]})
+  }
+
+  editItems = () => {
+    this.state.editMode ? 
+    this.setState({editMode: true}) : 
+    this.setState({editMode: true})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={this.handleClick}>TOGGLE ME</button>
+          <p>
+            {this.state.isClicked ? "true"  : "false"}
+          </p>
+          <p>Make a list:</p>
+          <form onSubmit={this.handleSubmit}>
+            <input type='text' value={this.state.inputValue} onChange={this.handleChange}></input>
+            <button type='sumbit'>sumbit</button>
+          </form>
+          <button onClick={() => this.editItems()}>Edit Items!</button>
+          <ol>{this.state.stringList.map((string, index) => {
+            return <li key={index}>{this.state.editMode ? <input onSubmit={this.handleSubmit} placeholder={string}></input> : string}
+              <button onClick={() => this.deleteItem(index)}>Delete Me!</button>
+            </li>
+          })}</ol>
+
+
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
