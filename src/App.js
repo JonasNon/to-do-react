1b8dc14f8d766c73ee0a7512d2058173abe525e3
JonasNon <officialjonasn@gmail.com>
2024-08-15 20:40:13 -0500

    spent way too long adding priority colors

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
       isClicked: false,
       inputValue: "",
       stringList: [],
-      editMode: false,
-      selectedItem: "placeholder"
+      colorList: [],
+      selectedItem: null,
+      color: "#2dc937"
     }
   }
   
@@ -28,49 +29,70 @@ class App extends Component {
     event.preventDefault()
     this.setState({stringList: [...this.state.stringList, this.state.inputValue]})
     this.setState({inputValue: ""})
+    this.setState({colorList: [...this.state.colorList, this.state.color]})
   }
   
   deleteItem = (index) => {
     let objectCopy = [...this.state.stringList]
     objectCopy.splice(index, 1)
-    console.log(objectCopy)
     this.setState({stringList: [...objectCopy]})
+    let colorCopy = [...this.state.colorList]
+    colorCopy.splice(index, 1)
+    this.setState({colorList: [...colorCopy]})
   }
 
-  editItems = () => {
-    this.state.editMode ? 
-    this.setState({editMode: false}) : 
-    this.setState({editMode: true})
-  }
+
 
   selectItem = (index, newTitle) => {
 
     
     this.state.selectedItem == index ? 
-    this.setState({stringList: [...this.state.stringList, this.state.newTitle]}) :
+    this.setState({selectedItem: "null"}) :
     this.setState({selectedItem: index})
+    console.log(this.state.selectedItem)
   }
 
+  // chooseColor = (div) => { //absolutely none of this works ;(
+  //   const colors = [document.getElementById("green"), document.getElementById("yellow"), document.getElementById("red")]
+  //   console.log(colors)
+  //   for (let i = 0; i < colors.length; i++) {
+  //     colors[i].classList.remove("selected")
+  //   }
+  //   document.getElementById(div).classList.add("selected")
+  // }
 
+  chooseColor = (colorString) => {
+    console.log(colorString)
+    if (colorString === "red") {
+      this.setState({color: "#cc3232"})
+    } else if (colorString === "yellow") {
+      this.setState({color: "#e7b416"})
+    } else if (colorString === "green") {
+      this.setState({color: "#2dc937"})
+    }
+    console.log(this.state.color)
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.props.name}</p>
-          <button onClick={this.handleClick}>TOGGLE ME</button>
-          <p>
-            {this.state.isClicked ? "true"  : "false"}
-          </p>
           <p>Make a list:</p>
           <form onSubmit={this.handleSubmit}>
+            <div className='color-selection' id='green' onClick={() => this.chooseColor("green")}></div> <div className='color-selection' id='yellow' onClick={() => this.chooseColor("yellow")}></div> <div className='color-selection' id='red' onClick={() => this.chooseColor("red")}></div> 
             <input type='text' value={this.state.inputValue} onChange={this.handleChange}></input>
             <button type='sumbit' onClick={this.handleSubmit}>sumbit</button>
           </form>
-          <button onClick={() => this.editItems()}>Edit Items!</button>
           <ol>{this.state.stringList.map((string, index) => {
-            return <TodoCard key={index} title={string} index={index} clickToRemove={this.deleteItem} clickToEdit={this.selectItem} selectedItem={this.state.selectedItem}/>
+            return <TodoCard 
+            key={index} 
+            title={string} 
+            index={index} 
+            clickToRemove={this.deleteItem} 
+            clickToEdit={this.selectItem} 
+            selectedItem={this.state.selectedItem} 
+            handleChange={this.handleChange} 
+            color={this.state.colorList[index]}/>
             
           })}</ol>
 
--- src/TodoCard.js
+++ src/TodoCard.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 
+const colorSelector = {
+  backgroundColor : "#e7b416",
+  position: "absolute",
+  left: "-2vh",
+  width: "5vw",
+  height: "8vh",
+  zIndex: "5"
+}
+
+
 function TodoCard (props) {
 
-  const {title, index, clickToRemove, clickToEdit, selectedItem} = props
+  const {title, index, clickToRemove, clickToEdit, selectedItem, handleChange, color} = props
 
-  const [color, changeColor] = useState("#2dc937")
 //	#2dc937, 	#e7b416, 	#cc3232
   return (
-  <div style={{backgroundColor: color}}>
-    <li>{index == selectedItem ? <input type="text" placeholder={title} onSubmit={clickToEdit(index)}></input> : title}
+  <div style={{backgroundColor: color, position: "relative", zIndex: "50"}}>
+    {/* <div style={colorSelector}></div> */}
+    <li>{index == selectedItem ? <input type="text" placeholder={title} onSubmit={clickToEdit(index)} onChange={handleChange}></input> : title}
       <div>
-      <button onClick={() => clickToRemove(index)}>Delete me!</button>
-      <button onClick={() => clickToEdit(index)}>Edit me!</button>
+        <button onClick={() => clickToRemove(index)}>Delete me!</button>
+        <button onClick={() => clickToEdit(index)}>Edit me!</button>
       </div>
     </li>
     
